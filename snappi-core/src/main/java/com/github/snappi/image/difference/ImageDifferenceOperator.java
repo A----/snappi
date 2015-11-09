7340a383a2695de183a2335cb9ef43d6830a7f7d
A---- <A----@users.noreply.github.com>
2015-11-09 23:06:29 +0100

    Difference operator

---
+++ snappi-core/src/main/java/com/github/snappi/image/difference/ImageDifferenceOperator.java
@@ -0,0 +1,56 @@
+package com.github.snappi.image.difference;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+/**
+ * Computes the difference between two images
+ */
+public class ImageDifferenceOperator {
+  /**
+   * Setting this for the current mask will ignore differences in pixels that are,
+   * in both images, not fully opaque.
+   */
+  private static int IGNORE_ALPHA_MASK = 0x00FF000000;
+  /**
+   * No pixel will be ignored.
+   */
+  private static int NO_MASK = 0x00000000;
+
+  /**
+   * Current mask.
+   * If, in both images, a pixel under that mask is not 0, it will be ignored.
+   * If only one of them is 0, it will count as different.
+   */
+  private int ignoreMask = IGNORE_ALPHA_MASK;
+
+  /**
+   * Does the math.
+   * See there: http://stackoverflow.com/questions/25022578/highlight-differences-between-images/25151302#25151302
+   * @param imageA Left operand.
+   * @param imageB right operand.
+   */
+  public long compute(BufferedImage imageA, BufferedImage imageB) {
+    long count = 0;
+
+    final int width = imageA.getWidth();
+    final int height = imageB.getHeight();
+
+    if (width != imageB.getWidth() || height != imageB.getHeight()) {
+      throw new IllegalArgumentException("Images does not have the same size.");
+    }
+
+    final int[] pixelsA = imageA.getRGB(0, 0, width, height, null, 0, width);
+    final int[] pixelsB = imageB.getRGB(0, 0, width, height, null, 0, width);
+
+    final int BLACK_RGB = Color.BLACK.getRGB();
+
+    for (int i = 0; i < pixelsA.length; i++) {
+      if (pixelsA[i] != pixelsB[i] && (pixelsA[i] & this.ignoreMask & pixelsB[i]) == BLACK_RGB) {
+        ++count;
+      }
+    }
+
+    return count;
+  }
+}
---
+++ snappi-core/src/test/java/com/github/snappi/image/difference/ImageDifferenceOperatorTest.java
@@ -0,0 +1,69 @@
+package com.github.snappi.image.difference;
+
+import static org.junit.Assert.assertEquals;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import org.junit.Test;
+
+public class ImageDifferenceOperatorTest {
+
+  public BufferedImage getImageFromResource(String fileName) {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    URL resource = classLoader.getResource(fileName);
+    if (resource == null) {
+      throw new IllegalArgumentException("Image could not be found.");
+    }
+
+    try {
+      File file = new File(classLoader.getResource(fileName).getFile());
+      return ImageIO.read(file);
+    }
+    catch(Exception e) {
+      throw new IllegalArgumentException("An error occured while reading the image.");
+    }
+  }
+
+  @Test
+  public void noDifferenceWithItself() {
+    BufferedImage lennaA = this.getImageFromResource("lenna.png");
+    BufferedImage lennaB = this.getImageFromResource("lenna.png");
+
+    ImageDifferenceOperator operator = new ImageDifferenceOperator();
+    long result = operator.compute(lennaA, lennaB);
+
+    assertEquals("There should not be any differences between an image and itself.", 0L, result);
+  }
+
+  @Test
+  public void someDifferencesExpected() {
+    BufferedImage lennaA = this.getImageFromResource("lenna.png");
+    BufferedImage lennaB = this.getImageFromResource("lenna-modified.png");
+
+    ImageDifferenceOperator operator = new ImageDifferenceOperator();
+    long result = operator.compute(lennaA, lennaB);
+
+    assertEquals("There are differences between those images.", 498L, result);
+  }
+
+  @Test
+  public void noDifferenceWithModifiedAlpha() {
+    BufferedImage lennaA = this.getImageFromResource("lenna-alpha.png");
+    BufferedImage lennaB = this.getImageFromResource("lenna-modified-alpha.png");
+
+    ImageDifferenceOperator operator = new ImageDifferenceOperator();
+    long result = operator.compute(lennaA, lennaB);
+
+    assertEquals("There should not be any differences when modifications are not fully opaque.", 0L, result);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void imagesOfDifferentSizes() {
+    BufferedImage lenna = this.getImageFromResource("lenna.png");
+    BufferedImage redPixel = this.getImageFromResource("red-pixel.png");
+
+    ImageDifferenceOperator operator = new ImageDifferenceOperator();
+    long result = operator.compute(lenna, redPixel);
+  }
+}
