593a433227876542276333cd3ea67d4809107147
A---- <A----@users.noreply.github.com>
2015-11-16 20:44:21 +0100

    Javadoc

--- snappi-file/src/main/java/snappi/loader/FileLoader.java
+++ snappi-file/src/main/java/snappi/loader/FileLoader.java
@@ -8,20 +8,36 @@ import javax.imageio.ImageIO;
 
 import snappi.image.Image;
 
+/**
+ * Collection of methods to load images from files.
+ * @see ResourceLoader
+ */
 public class FileLoader 
 {
-    public static Image load(String path) {
-      File file = new File(path);
-      return FileLoader.load(file);
+  /**
+   * Loads an image from its path.
+   * @param path Path to the file.
+   * @return The loaded image.
+   * @throws IllegalArgumentException If an error occurred or if the file does not exist.
+   */
+  public static Image load(String path) throws IllegalArgumentException {
+    File file = new File(path);
+    return FileLoader.load(file);
+  }
+  
+  /**
+   * Loads an image from a file.
+   * @param file The file.
+   * @return The loaded image.
+   * @throws IllegalArgumentException If an error occurred or if the file does not exist.
+   */
+  public static Image load(File file) throws IllegalArgumentException {
+    try {
+      BufferedImage bufferedImage = ImageIO.read(file); 
+      return new Image(bufferedImage);
     }
-    
-    public static Image load(File file) {
-      try {
-        BufferedImage bufferedImage = ImageIO.read(file); 
-        return new Image(bufferedImage);
-      }
-      catch (IOException e) {
-        throw new IllegalArgumentException("An error occured while reading the file", e);
-      }
+    catch (IOException e) {
+      throw new IllegalArgumentException("An error occured while reading the file", e);
     }
+  }
 }
--- snappi-junit/src/main/java/snappi/asserts/Assertions.java
+++ snappi-junit/src/main/java/snappi/asserts/Assertions.java
@@ -4,31 +4,83 @@ import snappi.image.Image;
 import snappi.image.difference.ImageDifferenceOperator;
 import snappi.image.difference.Result;
 
+/**
+ * Assertions on image similarity.
+ */
 public class Assertions {
-  static final String ASSERT_SAME_MESSAGE = "Actual image is different from the expected one.";
-  static final String ASSERT_SIMILAR_MESSAGE = "Actual image differs too much from the expected one.";
+  /**
+   * Default message when images should have been identical.
+   */
+  private static final String ASSERT_SAME_MESSAGE = "Actual image is different from the expected one.";
+  /**
+   * Default message when images are too different from one another.
+   */
+  private static final String ASSERT_SIMILAR_MESSAGE = "Actual image differs too much from the expected one.";
   
-  public static void assertSame(Image expected, Image actual) {
+  /**
+   * Image should be identical.
+   * @param expected Expected image.
+   * @param actual Actual image.
+   * @throws AssertionError When images are different.
+   */
+  public static void assertSame(Image expected, Image actual) throws AssertionError {
     assertSame(ASSERT_SAME_MESSAGE, expected, actual);
   }
   
-  public static void assertSame(String message, Image expected, Image actual) {
+  /**
+   * Image should be identical.
+   * @param message Description of the test.
+   * @param expected Expected image.
+   * @param actual Actual image.
+   * @throws AssertionError When images are different.
+   */
+  public static void assertSame(String message, Image expected, Image actual) throws AssertionError {
     assertSimilar(message, expected, actual, 0);
   }
 
-  public static void assertSimilar(Image expected, Image actual, int toleranceInPixels) {
+  /**
+   * Image should be similar
+   * @param expected Expected image.
+   * @param actual Actual image.
+   * @param toleranceInPixels Number of pixels of difference.
+   * @throws AssertionError When images are too different.
+   */
+  public static void assertSimilar(Image expected, Image actual, int toleranceInPixels) throws AssertionError {
     assertSimilar(ASSERT_SIMILAR_MESSAGE, expected, actual, toleranceInPixels);
   }
-  
-  public static void assertSimilar(String message, Image expected, Image actual, int toleranceInPixels) {
+
+  /**
+   * Image should be similar
+   * @param message Description of the test.
+   * @param expected Expected image.
+   * @param actual Actual image.
+   * @param toleranceInPixels Number of pixels of difference.
+   * @throws AssertionError When images are too different.
+   */
+  public static void assertSimilar(String message, Image expected, Image actual, int toleranceInPixels) throws AssertionError {
     assertSimilar(message, expected, actual, (long) toleranceInPixels);
   }
 
-  public static void assertSimilar(Image expected, Image actual, long toleranceInPixels) {
+  /**
+   * Image should be similar
+   * @param expected Expected image.
+   * @param actual Actual image.
+   * @param toleranceInPixels Number of pixels of difference.
+   * @throws AssertionError When images are too different.
+   */
+  public static void assertSimilar(Image expected, Image actual, long toleranceInPixels) throws AssertionError {
     assertSimilar(ASSERT_SIMILAR_MESSAGE, expected, actual, toleranceInPixels);
   }
-  
-  public static void assertSimilar(String message, Image expected, Image actual, long toleranceInPixels) {
+
+  /**
+   * Image should be similar
+   * @param message Description of the test.
+   * @param expected Expected image.
+   * @param actual Actual image.
+   * @param toleranceInPixels Number of pixels of difference.
+   * @throws AssertionError When images are too different.
+   */
+  public static void assertSimilar(String message, Image expected, Image actual, long toleranceInPixels) throws AssertionError {
     ImageDifferenceOperator operator = new ImageDifferenceOperator();
     Result result = operator.compute(expected, actual);
     
@@ -36,12 +88,27 @@ public class Assertions {
       throw new AssertionError(message + " [" + result + "]");
     }
   }
-  
-  public static void assertSimilar(Image expected, Image actual, double toleranceInPercentage) {
+
+  /**
+   * Image should be similar
+   * @param expected Expected image.
+   * @param actual Actual image.
+   * @param toleranceInPercentage Percentage of difference. {@code 1.0} is 1%. 
+   * @throws AssertionError When images are too different.
+   */
+  public static void assertSimilar(Image expected, Image actual, double toleranceInPercentage) throws AssertionError {
     assertSimilar(ASSERT_SIMILAR_MESSAGE, expected, actual, toleranceInPercentage);
   }
-  
-  public static void assertSimilar(String message, Image expected, Image actual, double toleranceInPercentage) {
+
+  /**
+   * Image should be similar
+   * @param message Description of the test.
+   * @param expected Expected image.
+   * @param actual Actual image.
+   * @param toleranceInPercentage Percentage of difference. {@code 1.0} is 1%. 
+   * @throws AssertionError When images are too different.
+   */
+  public static void assertSimilar(String message, Image expected, Image actual, double toleranceInPercentage) throws AssertionError {
     ImageDifferenceOperator operator = new ImageDifferenceOperator();
     Result result = operator.compute(expected, actual);
     
